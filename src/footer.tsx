dd67fb25dd0c59b3dc3db80053ddd0b21bfd3837
NAKAOKU Takahiro <giraffe_rakugaki@yahoo.co.jp>
2025-07-11 17:24:19 +0900

    フッターに、ヒロユニへのリンクを追加

--- src/footer.tsx
+++ src/footer.tsx
@@ -6,7 +6,9 @@ function Footer () {
   return (
         <Container>
           <footer className="footer1">
-            Copyleft 2025 Hiroshima Unity Study / Our resources are available under the &nbsp;
+            Copyleft 2025&nbsp;
+            <a href='https://hiroshima-unity.jimdofree.com'>Hiroshima Unity Study</a>
+            &nbsp;/ Our resources are available under the &nbsp;
             <a href='https://creativecommons.org/licenses/by-nc-sa/4.0/deed.ja'>Attribution-NonCommercial-ShareAlike 4.0 Creative Commons license</a>, except for parts we did not create.
           </footer>
         </Container>
